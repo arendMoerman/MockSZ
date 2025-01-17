c2f7d81f92ab4ebc82d2bddcd25d7e907bf9a1fe
arend <arendmoerman@gmail.com>
2025-01-17 17:07:15 +0100

    With updated docs

--- docs/search/all_3.js
+++ docs/search/all_3.js
@@ -1,9 +1,10 @@
 var searchData=
 [
-  ['calcsignal_5fksz_0',['calcSignal_kSZ',['../Signal_8cpp.html#a5ed8eaf41e0f475664001811dbfb3294',1,'calcSignal_kSZ(double mu, void *args):&#160;Signal.cpp'],['../Signal_8h.html#a5ed8eaf41e0f475664001811dbfb3294',1,'calcSignal_kSZ(double mu, void *args):&#160;Signal.cpp']]],
-  ['cluster_20models_1',['Cluster Models',['../group__clustermodels.html',1,'']]],
-  ['constants_2eh_2',['Constants.h',['../Constants_8h.html',1,'']]],
-  ['conv_5fcmb_5fscatt_3',['conv_CMB_scatt',['../Signal_8cpp.html#a28ac666b79f1a45d3af79fef60af17e4',1,'conv_CMB_scatt(double s, double *args):&#160;Signal.cpp'],['../Signal_8h.html#a28ac666b79f1a45d3af79fef60af17e4',1,'conv_CMB_scatt(double s, double *args):&#160;Signal.cpp']]],
-  ['conversions_2eh_4',['Conversions.h',['../Conversions_8h.html',1,'']]],
-  ['conversions_2epy_5',['Conversions.py',['../Conversions_8py.html',1,'']]]
+  ['calcsignal_5fcorrections_0',['calcSignal_corrections',['../Signal_8cpp.html#ac920c22816ed5880cb465d7d369929ce',1,'calcSignal_corrections(double nu, double Te, double beta_pec, double cosu):&#160;Signal.cpp'],['../Signal_8h.html#ac920c22816ed5880cb465d7d369929ce',1,'calcSignal_corrections(double nu, double Te, double beta_pec, double cosu):&#160;Signal.cpp']]],
+  ['calcsignal_5fksz_1',['calcSignal_kSZ',['../Signal_8cpp.html#a5ed8eaf41e0f475664001811dbfb3294',1,'calcSignal_kSZ(double mu, void *args):&#160;Signal.cpp'],['../Signal_8h.html#a5ed8eaf41e0f475664001811dbfb3294',1,'calcSignal_kSZ(double mu, void *args):&#160;Signal.cpp']]],
+  ['cluster_20models_2',['Cluster Models',['../group__clustermodels.html',1,'']]],
+  ['constants_2eh_3',['Constants.h',['../Constants_8h.html',1,'']]],
+  ['conv_5fcmb_5fscatt_4',['conv_CMB_scatt',['../Signal_8cpp.html#a28ac666b79f1a45d3af79fef60af17e4',1,'conv_CMB_scatt(double s, double *args):&#160;Signal.cpp'],['../Signal_8h.html#a28ac666b79f1a45d3af79fef60af17e4',1,'conv_CMB_scatt(double s, double *args):&#160;Signal.cpp']]],
+  ['conversions_2eh_5',['Conversions.h',['../Conversions_8h.html',1,'']]],
+  ['conversions_2epy_6',['Conversions.py',['../Conversions_8py.html',1,'']]]
 ];
--- docs/search/functions_2.js
+++ docs/search/functions_2.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['calcsignal_5fksz_0',['calcSignal_kSZ',['../Signal_8cpp.html#a5ed8eaf41e0f475664001811dbfb3294',1,'calcSignal_kSZ(double mu, void *args):&#160;Signal.cpp'],['../Signal_8h.html#a5ed8eaf41e0f475664001811dbfb3294',1,'calcSignal_kSZ(double mu, void *args):&#160;Signal.cpp']]],
-  ['conv_5fcmb_5fscatt_1',['conv_CMB_scatt',['../Signal_8cpp.html#a28ac666b79f1a45d3af79fef60af17e4',1,'conv_CMB_scatt(double s, double *args):&#160;Signal.cpp'],['../Signal_8h.html#a28ac666b79f1a45d3af79fef60af17e4',1,'conv_CMB_scatt(double s, double *args):&#160;Signal.cpp']]]
+  ['calcsignal_5fcorrections_0',['calcSignal_corrections',['../Signal_8cpp.html#ac920c22816ed5880cb465d7d369929ce',1,'calcSignal_corrections(double nu, double Te, double beta_pec, double cosu):&#160;Signal.cpp'],['../Signal_8h.html#ac920c22816ed5880cb465d7d369929ce',1,'calcSignal_corrections(double nu, double Te, double beta_pec, double cosu):&#160;Signal.cpp']]],
+  ['calcsignal_5fksz_1',['calcSignal_kSZ',['../Signal_8cpp.html#a5ed8eaf41e0f475664001811dbfb3294',1,'calcSignal_kSZ(double mu, void *args):&#160;Signal.cpp'],['../Signal_8h.html#a5ed8eaf41e0f475664001811dbfb3294',1,'calcSignal_kSZ(double mu, void *args):&#160;Signal.cpp']]],
+  ['conv_5fcmb_5fscatt_2',['conv_CMB_scatt',['../Signal_8cpp.html#a28ac666b79f1a45d3af79fef60af17e4',1,'conv_CMB_scatt(double s, double *args):&#160;Signal.cpp'],['../Signal_8h.html#a28ac666b79f1a45d3af79fef60af17e4',1,'conv_CMB_scatt(double s, double *args):&#160;Signal.cpp']]]
 ];
